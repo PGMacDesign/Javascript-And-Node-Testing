e1c77949e47986e25d6bf73961724dec2a7c0adf
Patrick MacDowell <PGMacDesign@gmail.com>
2018-12-17 12:59:03 -0800

    Adding in gitignore for node_modules folder as well as lodash package.

--- app1.js
+++ app1.js
@@ -9,19 +9,35 @@ const os = require('os');
 //Reference to constants.js file
 const myConstants = require('./Constants.js');
 
+//Reference to lodash
+const _ = require('lodash');
+
 var user = os.userInfo();
 var string_username = user.username;
 
+
+//For using the lodash module
 if(false){
-    console.log(user);
+    //Checks if the input value is a String
+    console.log(_.isString(true));
+    console.log(_.isString("a String"));
+
+    var filteredArray = _.uniq(["A String", true, "A String", 1, 1, 2, 3, 4]);
+    //Should be: "['A String', true, 1, 2, 3, 4]"
+    console.log(filteredArray);
 }
 
-var res = myConstants.addNote("patpat", true);
-console.log(res);
+//Utilizing the myConstants variable
+if(false){
+    console.log(user);
+    var res = myConstants.addNote("patpat", true);
+    console.log(res);
 
-var addedNums = myConstants.addValues(10, -1);
-console.log("addedNums = " +  addedNums)
+    var addedNums = myConstants.addValues(10, -1);
+    console.log("addedNums = " +  addedNums)
+}
 
+//File Writing
 if(false){
     //Synchronous method
     fs.appendFileSync('pat_text.txt', 'Hi ' + string_username + "!" + "\n"
